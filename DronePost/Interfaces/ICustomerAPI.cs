14cfafe67e19c0b688b856d98df75c4e74df2e64
Yaroslav <y.kovalskii@gmail.com>
2018-05-19 14:34:18 +0200

    ICusromerAPI, CustomerSimulator project added

---
+++ DronePost/Interfaces/ICustomerAPI.cs
@@ -0,0 +1,11 @@
+﻿using DronePost.DataModel;
+
+namespace DronePost.Interfaces
+{
+    public interface ICustomerAPI
+    {
+        void GetPackage(Package package);
+        void AddPackage(Package package, Station stationToAdd);
+        void ChangePersonalData();
+    }
+}
