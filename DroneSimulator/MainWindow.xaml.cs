b0a45a0c01dae115f51c59fd1b24f777f5f7b9b1
dliashuk <dlyashuk1997@gmail.com>
2018-05-19 20:22:19 +0200

    DroneSimulator Form

--- DroneSimulator/MainWindow.xaml.cs
+++ DroneSimulator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DronePost.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,36 @@ namespace DroneSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
+		private string modelName = null;
+		private int maxCarrySize = -1;
+		private float maxWeightSize = -1f;
+		private float maxFlightDistance = -1f;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
+
+		private void button_Click_1(object sender, RoutedEventArgs e)
+		{
+			Drone d = new Drone();
+			if (modelName != null && modelName != "" && maxCarrySize!=-1f && maxWeightSize!=-1f && maxFlightDistance!=-1f)
+			{
+				d.Model.ModelName = modelName;
+				d.Model.MaxSizeCarry = new PackageSize();
+				d.Model.MaxFlightDistance = maxFlightDistance;
+				d.Model.MaxWeightCarry = maxWeightSize;
+
+			}
+			DroneSimulation ds = new DroneSimulation(d);
+			MessageBox.Show("Drone is added");
+		}
+
+		private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+	}
 }
