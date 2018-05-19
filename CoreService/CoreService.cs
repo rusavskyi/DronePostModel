24cd503c2adc687c8943733bbcaf88c0da25afe7
Andrii Yereshchenko <telonily@gmail.com>
2018-05-19 18:44:57 +0200

    service

--- CoreService/CoreService.cs
+++ CoreService/CoreService.cs
@@ -4,29 +4,17 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using DronePost.Interfaces;
 
 namespace CoreService
 {
-    // Test of pushing
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CoreService" in both code and config file together.
     public class CoreService : ICoreService
     {
+        private ICore _core;
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
         }
     }
 }
--- CoreService/ICoreService.cs
+++ CoreService/ICoreService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DronePost.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -7,39 +8,10 @@ using System.Text;
 
 namespace CoreService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICoreService" in both code and config file together.
     [ServiceContract]
     public interface ICoreService
     {
         [OperationContract]
         string GetData(int value);
-
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
-    }
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "CoreService.ContractType".
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
