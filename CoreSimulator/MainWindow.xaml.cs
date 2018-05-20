919111d6dd198161bd63ba2b295938049f263471
DemiurgeUA <vrusavsky@gmail.com>
2018-05-21 00:46:55 +0200

    Core updates

--- CoreSimulator/MainWindow.xaml.cs
+++ CoreSimulator/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
-
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Windows;
+using CoreService;
 
 namespace CoreSimulator
 {
@@ -8,15 +11,62 @@ namespace CoreSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ServiceHost _host;
+
         public MainWindow()
         {
             InitializeComponent();
-            
         }
 
         public void WriteToOutput(string message)
         {
-            Dispatcher.Invoke(() => { TbOutput.AppendText(message + "\n"); });
+            Dispatcher.Invoke(() => { LogTextBox.AppendText(message + "\n"); });
+        }
+
+        private void StartHost()
+        {
+            Uri baseAddress = new Uri("http:/localhost:8888/Core");
+            _host = new ServiceHost(typeof(CoreService.CoreService), baseAddress);
+
+            try
+            {
+                ServiceEndpoint endpoint = _host.AddServiceEndpoint(
+                    typeof(ICoreService),
+                    new BasicHttpBinding(),
+                    ""
+                    );
+                ServiceMetadataBehavior bechavior = new ServiceMetadataBehavior()
+                {
+                    HttpGetEnabled = true
+                };
+                _host.Description.Behaviors.Add(bechavior);
+
+                _host.Open();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private void StopHost()
+        {
+            _host.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopHost();
+        }
+
+        private void StartSimButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartHost();
+        }
+
+        private void StopSimButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopHost();
         }
     }
 }
