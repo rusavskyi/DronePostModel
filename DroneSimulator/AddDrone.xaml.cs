b0d1091a5db95aa120d75204fa5801f82a6af820
dliashuk <dlyashuk1997@gmail.com>
2018-05-21 00:27:34 +0200

    Drone Sim Windows

---
+++ DroneSimulator/AddDrone.xaml.cs
@@ -0,0 +1,76 @@
+﻿using DronePost.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DroneSimulator
+{
+	/// <summary>
+	/// Interaction logic for AddDrone.xaml
+	/// </summary>
+	public partial class AddDrone : Window
+	{
+		private string modelName = null;
+		private int maxCarrySize = -1;
+		private float maxWeightSize = -1f;
+		private float maxFlightDistance = -1f;
+		private List<DroneSimulation> droneList = new List<DroneSimulation>();
+		public AddDrone()
+		{
+			InitializeComponent();
+
+			comboBoxMaxSizeCarry.Items.Insert(0, "A");
+			comboBoxMaxSizeCarry.Items.Insert(1, "B");
+			comboBoxMaxSizeCarry.Items.Insert(2, "C");
+			comboBoxMaxSizeCarry.Items.Insert(3, "D");
+		}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+		
+			Drone d = new Drone();
+			
+			try
+			{
+				maxWeightSize = float.Parse(textBoxMaxWeightCarry.Text);
+				maxFlightDistance = float.Parse(textBoxMaxFlightDistance.Text);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Float conver exeption");
+			}
+			if (modelName != null && modelName != "" && maxCarrySize != -1 && maxWeightSize != -1f && maxFlightDistance != -1f)
+			{
+
+				d.Model.ModelName = modelName;
+				d.Model.MaxSizeCarry = new PackageSize();
+				d.Model.MaxFlightDistance = maxFlightDistance;
+				d.Model.MaxWeightCarry = maxWeightSize;
+
+			}
+			DroneSimulation ds = new DroneSimulation(d);
+			droneList.Add(ds);
+
+
+
+
+			textBoxModelName.Text = "";
+			textBoxMaxFlightDistance.Text = "";
+			textBoxMaxWeightCarry.Text = "";
+			comboBoxMaxSizeCarry.SelectedIndex = 0;
+			MessageBox.Show("Drone is added");
+			MainWindow mw = new MainWindow();
+			mw.Show();
+		}
+	}
+}
--- DroneSimulator/MainWindow.xaml.cs
+++ DroneSimulator/MainWindow.xaml.cs
@@ -6,11 +6,11 @@ using System.Windows.Controls;
 
 namespace DroneSimulator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
 		private string modelName = null;
 		private int maxCarrySize = -1;
 		private float maxWeightSize = -1f;
@@ -18,87 +18,22 @@ namespace DroneSimulator
 		private List<DroneSimulation> droneList = new List<DroneSimulation>();
 
 
-        public MainWindow()
-        {
-            InitializeComponent();
-			comboBoxMaxSizeCarry.Items.Insert(0, "A");
-			comboBoxMaxSizeCarry.Items.Insert(1, "B");
-			comboBoxMaxSizeCarry.Items.Insert(2, "C");
-			comboBoxMaxSizeCarry.Items.Insert(3, "D");
-
-			comboBoxDroneTask.Items.Insert(0, "TakePackage");
-			comboBoxDroneTask.Items.Insert(1, "GoToStation");
-			comboBoxDroneTask.Items.Insert(2, "LeavePackage");
-			comboBoxDroneTask.Items.Insert(3, "ChargeAtStation");
-
-			comboBoxStation.Items.Insert(0, "StationA");
-			comboBoxStation.Items.Insert(0, "StationB");
-			comboBoxStation.Items.Insert(0, "StationC");
-			comboBoxStation.Items.Insert(0, "StationD");
-			comboBoxStation.Items.Insert(0, "StationE");
-			comboBoxStation.Items.Insert(0, "StationF");
-		}
-
-		private void button_Click_1(object sender, RoutedEventArgs e)
+		public MainWindow()
 		{
-			modelName = textBoxModelName.Text;
-			Drone d = new Drone();
-			comboBoxDrone.Items.Add(modelName);
-			try { 
-				maxWeightSize = float.Parse(textBoxMaxWeightCarry.Text);
-				maxFlightDistance = float.Parse(textBoxMaxFlightDistance.Text);
-			}
-			catch (Exception ex) {
-				MessageBox.Show("Float conver exeption");
-			}
-			if (modelName != null && modelName != "" && maxCarrySize!=-1 && maxWeightSize!=-1f && maxFlightDistance!=-1f)
-			{
-				
-				d.Model.ModelName = modelName;
-				d.Model.MaxSizeCarry = new PackageSize();
-				d.Model.MaxFlightDistance = maxFlightDistance;
-				d.Model.MaxWeightCarry = maxWeightSize;
-
-			}
-			DroneSimulation ds = new DroneSimulation(d);
-			droneList.Add(ds);
-			
-				
-
-
-			textBoxModelName.Text = "";
-			textBoxMaxFlightDistance.Text = "";
-			textBoxMaxWeightCarry.Text = "";
-			comboBoxMaxSizeCarry.SelectedIndex = 0;
-			MessageBox.Show("Drone is added");
-		}
-
-		private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
+			InitializeComponent();
 
 		}
 
-		private void comboBoxMaxSizeCarry_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		private void buttonAddDront_Click(object sender, RoutedEventArgs e)
 		{
-
+			AddDrone ad = new AddDrone();
+			ad.Show();
 		}
 
-		private void comboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+		private void buttonAddTask_Click(object sender, RoutedEventArgs e)
 		{
-
-		}
-
-		private void comboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
-		{
-
-		}
-
-		private void button_Click(object sender, RoutedEventArgs e)
-		{
-			comboBoxDrone.SelectedIndex = 0;
-			comboBoxDroneTask.SelectedIndex = 0;
-			comboBoxStation.SelectedIndex = 0;
-			MessageBox.Show("Task is added to queue");
+			AddDroneTask adt = new AddDroneTask();
+			adt.Show();
 		}
 	}
 }
