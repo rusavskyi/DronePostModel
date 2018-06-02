7925d00bce772dcc20cb53eb7ce55f5428e6310e
DemiurgeUA <vrusavsky@gmail.com>
2018-06-03 00:30:15 +0200

    Fixes

--- CoreSimulator/Properties/Settings.Designer.cs
+++ CoreSimulator/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Core.Properties {
+namespace CoreHost.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
