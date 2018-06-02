4f295429aba970dd26a079809eaa4367a09b269c
DemiurgeUA <vrusavsky@gmail.com>
2018-06-02 21:10:02 +0200

    PopulateCustomers

--- CoreSimulator/Migrations/201806021253362_PopulateCustomer.cs
+++ CoreSimulator/Migrations/201806021253362_PopulateCustomer.cs
@@ -8,8 +8,24 @@ namespace CoreHost
         public override void Up()
         {
             Sql(@"INSERT INTO Customers (CompanyAgent, FirstName, LastName, BirthDate, Address, Address2, City_Id, Company_Id)
-                VALUES (),
-               ");
+                VALUES (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 1, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 1, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 1, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 1, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 2, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 2, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 2, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 2, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 3, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 3, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 4, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 4, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 1, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 5, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 2, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 1, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 3, NULL),
+                       (0, 'GenericSurname', 'GenericName', '1/01/1975', 'Generic Address', '', 4, NULL)");
         }
         
         public override void Down()
