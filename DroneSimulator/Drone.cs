66f42cd311e9cd9c028b7876480f257c79bce0d8
Andrii Yereshchenko <telonily@gmail.com>
2018-06-03 18:03:53 +0200

    add Drone class, work around

---
+++ DroneSimulator/Drone.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DronePost.Interfaces;
+using DronePost.SupportClasses;
+
+namespace DroneSimulator
+{
+    public class Drone : DronePost.DataModel.Drone, IDrone
+    {
+        public DroneTechInfo GetTechInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTask(DroneTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetTask(DroneTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoNextTask(bool force = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
